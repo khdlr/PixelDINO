59801d89402b63a7e91f0291055219ca53ef3648
Konrad Heidler <k.heidler@tum.de>
2022-11-30 10:38:33 +0100

    More test augs

--- augmentations_test.py
+++ augmentations_test.py
@@ -20,11 +20,11 @@ def rgb(batch, i):
 
 def mask(batch, i):
   image = batch['Mask'][i]
+  image = np.where(image > 2, 0, image)
   uint8 = np.clip(255 * image, 0, 255).astype(np.uint8)
   rgb = np.concatenate([uint8]*3, axis=2)
   rgb = np.asarray(rgb)
   return Image.fromarray(rgb)
-
 
 
 if __name__ == '__main__':
@@ -34,22 +34,27 @@ if __name__ == '__main__':
     @jax.jit
     def augment(batch):
       val_prepped = utils.prep(batch)
-      weakly_prepped = utils.prep(batch, jax.random.PRNGKey(129837))
-      strongly_prepped = utils.distort(weakly_prepped, jax.random.PRNGKey(87298))
+      weakly_prepped = utils.prep(batch, jax.random.PRNGKey(12890))
+      strongly_prepped = utils.distort(weakly_prepped, jax.random.PRNGKey(189))
       return val_prepped, weakly_prepped, strongly_prepped
 
+    jj = 0
     for B, meta in data_trn:
       V, W, S = augment(B)
       for i in range(B['Sentinel2'].shape[0]):
         ary = V['Sentinel2'][i]
-        print(i, ary.min(), ary.mean(), ary.max())
-        rgb(V, i).save(f'augs/{i}_raw_rgb.jpg')
-        mask(V, i).save(f'augs/{i}_raw_mask.png')
-        rgb(W, i).save(f'augs/{i}_weak_rgb.jpg')
-        mask(W, i).save(f'augs/{i}_weak_mask.png')
-        rgb(S, i).save(f'augs/{i}_strong_rgb.jpg')
-        mask(S, i).save(f'augs/{i}_strong_mask.png')
+        print(jj, 'raw', ary.min(), ary.mean(), ary.max())
+        ary = W['Sentinel2'][i]
+        print(jj, 'aug', ary.min(), ary.mean(), ary.max())
+        rgb( V, i).save(f'augs/{jj:03d}_raw_rgb.jpg')
+        mask(V, i).save(f'augs/{jj:03d}_raw_mask.png')
+        rgb( W, i).save(f'augs/{jj:03d}_weak_rgb.jpg')
+        mask(W, i).save(f'augs/{jj:03d}_weak_mask.png')
+        rgb( S, i).save(f'augs/{jj:03d}_strong_rgb.jpg')
+        mask(S, i).save(f'augs/{jj:03d}_strong_mask.png')
+        jj += 1
+      if jj > 256:
+        break
 
-      break
 
 
